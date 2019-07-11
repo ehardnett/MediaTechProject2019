125c43d51266419b9469b0cd857b562c8f55d212
206623174 <erin.hardnett@nbcuni.com>
2019-07-11 13:23:15 -0400

    Working on the peacock; sections divided for the website

--- js/mediatech.js
+++ js/mediatech.js
@@ -1,3 +1,52 @@
+
+
+$(document).ready(function($){
+    var parPosition = [];
+  $('.par').each(function() {
+      parPosition.push($(this).offset().top);
+  });
+
+  $('.vNav ul li a').click(function () {
+    $('.vNav ul li a').removeClass('active');
+        $(this).addClass('active');
+}); 
+
+$('.vNav a').hover(function() {
+   $(this).find('.label').show();
+   }, function() {
+   $(this).find('.label').hide();
+});
+
+  $('.vNav a').click(function(){
+      $('.states').animate({
+          scrollTop:  $( $.attr(this, 'href'.substring(1)) ).offset().top
+      }, 500);
+      return false;
+  });
+  
+     
+  
+     $('.states').scroll(function(){
+  var position = $('.states').scrollTop(),
+          index; 
+          for (var i=0; i<parPosition.length; i++) {
+          if (position <= parPosition[i]) {
+              index = i;
+              break;
+          }
+      }
+$('.vNav ul li a').removeClass('active');
+      $('.vNav ul li a:eq('+index+')').addClass('active');
+  });
+  
+      $('.vNav ul li a').click(function () {
+      $('.vNav ul li a').removeClass('active');
+          $(this).addClass('active');
+  });   
+});
+
+
+
 function showDiv(id, collapse1, collapse2, collapse3) {
     var x = document.getElementById(id);
     if (window.getComputedStyle(x).display === "none") {
