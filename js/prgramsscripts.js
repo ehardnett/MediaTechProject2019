f66c1ab768f8bedc1c3a8c6d934c14780acc12a1
206623166 <Parker.Fisher@nbcuni.com>
2019-07-24 11:23:14 -0400

    fix for intern/timeline interaction v2

--- js/prgramsscripts.js
+++ js/prgramsscripts.js
@@ -1,41 +1,40 @@
 // When the user clicks on div, open the popup
 function myFunction() {
-  var popup = document.getElementById("myPopup");
-  popup.classList.toggle("show");
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
 }
 
 
-function PopUp ()
-{
+function PopUp() {
     var popup2 = document.getElementById("myPopup2");
     popup2.classList.toggle("show2")
 }
 
-$( function grow() {
-    $( ".internship" ).on( "click", function() {
-      $( "#effect" ).toggleClass( "newClass", 1000, "linear" );
- document.getElementById("associate").style.left = "0px";
+$(function grow() {
+    $(".internship").on("click", function() {
+        $("#effect").toggleClass("newClass", 1000, "linear");
+        document.getElementById("associate").style.left = "0px";
 
     });
- 
+
     function callback() {
-      setTimeout(function() {
-        $( "#effect" ).removeClass( "newClass" );
-      }, 2000 );
+        setTimeout(function() {
+            $("#effect").removeClass("newClass");
+        }, 2000);
     }
 
-  });
+});
 
-$( function grow2() {
-    $( ".associates" ).on( "click", function() {
-      $( "#effect2" ).toggleClass( "newClass2", 1000, "linear" );
- document.getElementById("associate").style.left = "0px";
+$(function grow2() {
+    $(".associates").on("click", function() {
+        $("#effect2").toggleClass("newClass2", 1000, "linear");
+        document.getElementById("associate").style.left = "0px";
 
     });
- 
+
     function callback2() {
-      setTimeout(function() {
-        $( "#effect2" ).removeClass( "newClass2" );
-      }, 2000 );
-}
-  });
+        setTimeout(function() {
+            $("#effect2").removeClass("newClass2");
+        }, 2000);
+    }
+});
